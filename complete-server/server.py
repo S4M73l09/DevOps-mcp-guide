feaa664aa7933ce70124e0f55a2854ff86ddac52
S4M73l09 <sam1999xxx@gmail.com>
2026-09-18 15:59:44 +0200

    server.py with tools Kubernetes added

--- complete-server/server.py
+++ complete-server/server.py
@@ -5,7 +5,7 @@ from config import load_config
 from tools.diagnostics import register_diagnostic_tools
 from tools.terraform import register_terraform_tools
 from tools.docker import register_docker_tools
-#from tools.kubernetes import register_kubernetes_tools
+from tools.kubernetes import register_kubernetes_tools
 #from resources.infrastructure import register_resources
 #from prompts.operations import register_prompts
 
@@ -19,7 +19,7 @@ mcp = MCPServer(config.name)
 register_diagnostic_tools(mcp, config)
 register_terraform_tools(mcp, config)
 register_docker_tools(mcp, config)
-#register_kubernetes_tools(mcp, config)
+register_kubernetes_tools(mcp, config)
 
 
 #register_resources(mcp, config)
